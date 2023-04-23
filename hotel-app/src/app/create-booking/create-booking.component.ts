e5aa6683a9ade6bda618c0c0197a315491de2a30
DKirsch <eistitan@yahoo.com>
2023-04-23 21:41:09 +0200

    click event and navigate

--- hotel-app/src/app/create-booking/create-booking.component.ts
+++ hotel-app/src/app/create-booking/create-booking.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Booking } from '../booking';
+import { Bookings } from '../mock-bookings';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-booking',
@@ -7,11 +9,20 @@ import { Booking } from '../booking';
   styleUrls: ['./create-booking.component.css']
 })
 export class CreateBookingComponent {
+
+  constructor(private router: Router) {} //gibt dem Konstruktor das Router-Objekt
+
   booking: Booking = {
-    id: 100,
-    name: "Test Name",
-    roomNumber: 100,
+    id: 0,
+    name: "",
+    roomNumber: 0,
     startDate: new Date(),
     endDate: new Date()
+  }
+
+  save(): void {
+    Bookings.push(this.booking); //neuer Eintrag in die MockListe
+    console.log("SAVED");
+    this.router.navigate(['bookings']); //Navigiert den User zur Übersicht
   }
 }
